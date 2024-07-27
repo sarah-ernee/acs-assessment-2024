cb80768baa820ca7e272736c80438d2abbb7ed5a
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-07-27 21:45:13 +0800

    optimized navbar for mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Menu, X, Search } from "lucide-react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="title">AEON</div>
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const checkScreenSize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    // Clear event listeners on rerender
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+
+    // Hide search bar first
+    setIsSearchOpen(false);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  const renderNavItems = () => (
+    <>
       <ul>
         <li>
           <a href="/">Showcase</a>
@@ -25,10 +52,45 @@ const Navbar = () => {
           <a href="/">Enterprise</a>
         </li>
       </ul>
+      {!isMobile && (
+        <div className="search-bar">
+          <input type="text" placeholder="Search documentation..." />
+        </div>
+      )}
+    </>
+  );
 
-      <div className="search-bar">
-        <input type="text" placeholder="Search documentation..." />
+  return (
+    <nav className="navbar">
+      <div className="navbar-header">
+        <div className="title">AEON</div>
+        {isMobile && (
+          <div className="mobile-icons">
+            {/* Only show hamburger icon if mobile navbar not expanded */}
+            {!isOpen && <Menu size={24} onClick={toggleMenu} />}
+
+            {/* Show icons once mobile navbar expanded */}
+            {isOpen && (
+              <>
+                {isSearchOpen ? (
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="mobile-search-input"
+                  />
+                ) : (
+                  <Search size={20} onClick={toggleSearch} />
+                )}
+                <X size={24} onClick={toggleMenu} />
+              </>
+            )}
+          </div>
+        )}
       </div>
+
+      {/* Outright render ul items if in laptop view */}
+      {/* Render ul items if mobile and expanded */}
+      {!isMobile || isOpen ? renderNavItems() : null}
     </nav>
   );
 };
