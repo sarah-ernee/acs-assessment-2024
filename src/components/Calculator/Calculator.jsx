612c7c24b8b0afa7dc1fdbcb01ac832803d0f392
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-07-28 11:43:01 +0800

    cleaned up code

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -4,7 +4,7 @@ import "./Calculator.css";
 const Calculator = () => {
   const [firstNum, setFirstNum] = React.useState();
   const [secondNum, setSecondNum] = React.useState();
-  const [sum, setSum] = React.useState(0);
+  const [sum, setSum] = React.useState();
   const [invalidInput, setInvalidInput] = React.useState(false);
 
   const addNum = () => {
@@ -20,6 +20,7 @@ const Calculator = () => {
     <div className="calculator">
       <h1>Adding Two Numbers</h1>
 
+      {/* Affirm number type with +e */}
       <input
         type="number"
         value={firstNum}
@@ -42,13 +43,15 @@ const Calculator = () => {
       />
 
       <button onClick={() => addNum()}>Add Two Numbers</button>
+
+      {/* Simple check that there are two inputs for sum to work */}
       {invalidInput ? (
         <h5 className="warning">Please enter two numbers to sum</h5>
       ) : (
         ""
       )}
 
-      <p>Total: {sum > 0 ? sum : ""}</p>
+      <p>Total: {sum}</p>
     </div>
   );
 };
